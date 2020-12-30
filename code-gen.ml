213743494709514b5c14edac9a9373bd9b614f77
tomersiv <stk112233@gmail.com>
2020-12-30 13:09:58 +0200

    continue generate

--- code-gen.ml
+++ code-gen.ml
@@ -29,40 +29,14 @@ module type CODE_GEN = sig
      by the semantic analyser.
    *)
   val generate : (constant * (int * string)) list -> (string * int) list -> expr' -> string
+  
+  (*TODO remove*)
+  val func : string -> expr'
 end;;
 
 exception X_syntax_error;;
 
 module Code_Gen : CODE_GEN = struct
-let make_consts_tbl asts = let const_list = List.flatten(List.map (fun ast -> create_const_list ast []) asts) in
-                           let const_list = remove_duplicates const_list in
-                           let const_list = [Void; Sexpr(Nil); Sexpr(Bool(false)); Sexpr(Bool(true))] @ const_list in
-                           let const_table = create_const_tbl const_list [] 0 in
-                           const_table ;;  
-
-let rec create_const_list ast const_list = match ast with 
-| If'(test, dit, dif) -> (create_const_list test const_list) @ (create_const_list dit const_list) @ (create_const_list dif const_list)  
-| Def'(var, value) -> create_const_list value const_list
-| Set'(var, value) -> create_const_list value const_list
-| Seq'(exprs) -> List.flatten(List.map (fun expr -> create_const_list expr const_list) exprs)
-| Or'(exprs) -> List.flatten(List.map (fun expr -> create_const_list expr const_list) exprs)
-| LambdaSimple'(args, body) ->  create_const_list body const_list
-| LambdaOpt'(args,optArg, body) -> create_const_list body const_list
-| Applic'(rator, rands) -> (create_const_list rator const_list) @ (List.flatten(List.map (fun rand -> create_const_list rand const_list)) rands)
-| ApplicTP'(rator, rands) -> (create_const_list rator const_list) @ (List.flatten(List.map (fun rand -> create_const_list rand const_list)) rands) 
-| BoxSet'(var, value) -> create_const_list value const_list
-| Const'(x) -> (match x with
-                | Void -> const_list
-                | Sexpr(expr) -> (match expr with
-                                  | Nil -> const_list
-                                  | Bool(x) -> const_list
-                                  | Symbol(str) -> [Sexpr(String(str)); Sexpr(expr)] @ const_list
-                                  | Pair(first, res) -> (create_const_list Const'(Sexpr(first)) const_list) @ (create_const_list Const'(Sexpr(rest)) const_list) @ [Sexpr(Pair(first, res))] 
-                                  | _ -> [Sexpr(expr)] @ const_list
-                )
-                | _ -> const_list
-)
-| _ -> const_list
 
 (*TODO change*)
 let rec find_offset const_table exp = 
@@ -70,6 +44,7 @@ let rec find_offset const_table exp =
       | (Sexpr(expr),(offset,tag)) :: rest -> if sexpr_eq exp expr then offset else find_offset rest exp
       | (Void, (offset, tag)) :: rest -> find_offset rest exp
       | [] -> 0 ;;
+
 
 let rec create_const_tbl const_list const_table offset = 
 match const_list with
@@ -79,12 +54,11 @@ match const_list with
                   | Sexpr(Bool(false)) -> (create_const_tbl rest (const_table @ [(Sexpr(Bool(false)), (offset, "MAKE_BOOL(0)"))]) (offset + 2)) 
                   | Sexpr(Bool(true)) -> (create_const_tbl rest (const_table @ [(Sexpr(Bool(true)), (offset, "MAKE_BOOL(1)"))]) (offset + 2)) 
                   | Sexpr(Char(ch)) -> (create_const_tbl rest (const_table @ [(Sexpr(Char(ch)), (offset, "MAKE_LITERAL_CHAR("^(string_of_int (Char.code ch))^")"))]) (offset + 2))                                                         
-                  | Sexpr(Number(Fraction(x, y))) -> (create_const_tbl rest (const_table @ [(Sexpr(Number(Fraction(x, y))), (offset, "MAKE_LITERAL_RATIONAL(" ^ (string_of_int x) ^ "," ^ (string_of_int y) ^ ")\n"))]) (offset + 17))                                           
+                  | Sexpr(Number(Fraction(x, y))) -> (create_const_tbl rest (const_table @ [(Sexpr(Number(Fraction(x, y))), (offset, "MAKE_LITERAL_RATIONAL(" ^ (string_of_int x) ^ "," ^ (string_of_int y) ^ ")"))]) (offset + 17))                                           
                   | Sexpr(Number(Float(x))) -> (create_const_tbl rest (const_table @ [(Sexpr(Number(Float(x))), (offset, "MAKE_LITERAL_FLOAT(" ^ (string_of_float x) ^ ")"))]) (offset + 9))
                   | Sexpr(String(str)) -> (create_const_tbl rest (const_table @ [(Sexpr(String(str)), (offset, "MAKE_LITERAL_STRING(" ^ str ^ ")"))]) (offset + 9 + (String.length str)))
                   | Sexpr(Symbol(str)) -> (create_const_tbl rest (const_table @ [(Sexpr(Symbol(str)), (offset, "MAKE_LITERAL_SYMBOL(const_tbl+"^(string_of_int (find_offset const_table (String(str))))^")"))]) (offset + 9))
-                  | Pair(car, cdr) -> (create_const_tbl rest (const_table @ [(Sexpr(Pair(car, cdr)) , (offset , "MAKE_LITERAL_PAIR(const_tbl+"^(string_of_int (find_offset const_table car))^", const_tbl+"^(string_of_int (find_offset const_table car))^")"))]) (offset + 17))
-                  | _ -> create_const_list rest const_table offset
+                  | Sexpr(Pair(car, cdr)) -> (create_const_tbl rest (const_table @ [(Sexpr(Pair(car, cdr)) , (offset , "MAKE_LITERAL_PAIR(const_tbl+"^(string_of_int (find_offset const_table car))^", const_tbl+"^(string_of_int (find_offset const_table car))^")"))]) (offset + 17))
 )
 | [] -> const_table ;; 
 
@@ -94,11 +68,45 @@ let rec remove_duplicates lst = match lst with
                               | [] -> lst
                               | curr :: rest -> if(List.mem curr rest) then (remove_duplicates rest) else curr :: (remove_duplicates rest) 
 
+
+let rec create_const_list ast const_list = match ast with 
+| If'(test, dit, dif) -> (create_const_list test const_list) @ (create_const_list dit const_list) @ (create_const_list dif const_list)  
+| Def'(var, value) -> create_const_list value const_list
+| Set'(var, value) -> create_const_list value const_list
+| Seq'(exprs) -> List.flatten(List.map (fun expr -> create_const_list expr const_list) exprs)
+| Or'(exprs) -> List.flatten(List.map (fun expr -> create_const_list expr const_list) exprs)
+| LambdaSimple'(args, body) ->  create_const_list body const_list
+| LambdaOpt'(args,optArg, body) -> create_const_list body const_list
+| Applic'(rator, rands) -> (create_const_list rator const_list) @ (List.flatten(List.map (fun rand -> (create_const_list rand const_list))rands))
+| ApplicTP'(rator, rands) -> (create_const_list rator const_list) @ (List.flatten(List.map (fun rand -> create_const_list rand const_list)rands)) 
+| BoxSet'(var, value) -> create_const_list value const_list
+| Const'(x) -> (match x with
+                | Void -> const_list
+                | Sexpr(expr) -> (match expr with
+                                  | Nil -> const_list
+                                  | Bool(x) -> const_list
+                                  | Symbol(str) -> [Sexpr(String(str)); Sexpr(expr)] @ const_list
+                                  | Pair(car, cdr) -> (create_const_list (Const'(Sexpr(car))) const_list) @ (create_const_list (Const'(Sexpr(cdr))) const_list) @ [Sexpr(Pair(car, cdr))] 
+                                  | _ -> [Sexpr(expr)] @ const_list
+                )
+)
+| _ -> const_list
+
+let make_consts_tbl asts = let const_list = List.flatten(List.map (fun ast -> create_const_list ast []) asts) in
+                           let const_list = remove_duplicates const_list in
+                           let const_list = [Void; Sexpr(Nil); Sexpr(Bool(false)); Sexpr(Bool(true))] @ const_list in
+                           let const_table = create_const_tbl const_list [] 0 in
+                           const_table ;;  
+
+
+
+
+
 let rec create_fvar_list ast fvar_list =
 match ast with 
 | If'(test, dit, dif) -> (create_fvar_list test fvar_list) @ (create_fvar_list dit fvar_list) @ (create_fvar_list dif fvar_list) 
-| Def'(Var'(VarFree(varname)), value) -> (create_fvar_list value (fvar_list @ [varname]))
-| Set'(Var'(var), value) -> (match var with
+| Def'(VarFree(varname), value) -> (create_fvar_list value (fvar_list @ [varname]))
+| Set'(var, value) -> (match var with
                             | VarFree(varname) -> (create_fvar_list value (fvar_list @ [varname]))
                             | _ -> (create_fvar_list value fvar_list)
 )
@@ -106,8 +114,8 @@ match ast with
 | Or'(exprs) -> List.flatten(List.map (fun expr -> create_fvar_list expr fvar_list) exprs)
 | LambdaSimple'(args, body) ->  create_fvar_list body fvar_list
 | LambdaOpt'(args,optArg, body) -> create_fvar_list body fvar_list
-| Applic'(rator, rands) -> (create_fvar_list rator fvar_list) @ (List.flatten(List.map (fun rand -> create_fvar_list rand fvar_list)) rands)
-| ApplicTP'(rator, rands) -> (create_fvar_list rator fvar_list) @ (List.flatten(List.map (fun rand -> create_fvar_list rand fvar_list)) rands) 
+| Applic'(rator, rands) -> (create_fvar_list rator fvar_list) @ (List.flatten(List.map (fun rand -> create_fvar_list rand fvar_list)rands))
+| ApplicTP'(rator, rands) -> (create_fvar_list rator fvar_list) @ (List.flatten(List.map (fun rand -> create_fvar_list rand fvar_list)rands)) 
 | BoxSet'(var, value) -> (match var with 
                          | VarFree(varname) -> (create_fvar_list value (fvar_list @ [varname]))
                          | _ -> (create_fvar_list value fvar_list)
@@ -156,6 +164,14 @@ match fvar_table with
 | [] -> raise X_syntax_error
                            
 
+let label_counter_Gen label =
+  let counter = ref (-1) 
+  in
+  fun ToIncrease ->
+    if ToIncrease
+    then incr counter;
+    label ^ string_of_int !counter
+;;
 
 let rec generate_code consts fvars e depth = 
 match e with
@@ -182,19 +198,28 @@ match e with
                                                                   mov rax, SOB_VOID_ADDRESS"
 )
 | Seq'(exprs) -> String.concat "\n" (List.map (fun expr -> (generate_code consts fvars expr depth)) exprs)
-| Or'(exprs) ->                                    
-                           
+| Or'(exprs) -> let label = label_counter_Gen "" in
+                String.concat ("\n cmp rax, SOB_FALSE_ADDRESS\n jne Lexit"^label^"\n") 
+                (List.map (fun expr -> (generate_code consts fvars expr depth)) exprs)^"Lexit"^label^":"
+| If'(test, dit, dif) -> let label_else = label_counter_Gen "" in
+                        let lable_exit = label_counter_Gen "" in 
+                        (generate_code consts fvars test depth)^"\n cmp rax, SOB_FALSE_ADDRESS\n je Lelse"^label_else^"\n"
+                        ^(generate_code consts fvars dit depth)^"\n jmp Lexit"^lable_exit^"\n Lelse"^label_else^":"
+                        ^(generate_code consts fvars dif depth)^ "\n Lexit"^lable_exit^":" ;;             
+                                  
+                          
 
 
 
 
                 
-
- 
+(*TODO remove*)
+let func str = (Semantics.run_semantics (List.hd(Tag_Parser.tag_parse_expressions (Reader.read_sexprs str))));;
   
 
 
 
-let generate consts fvars e = generate_code consts fvars e 0 ;;
+let generate consts fvars e = raise X_syntax_error;;(*generate_code consts fvars e 0 ;;
+*)
 end;;
 
